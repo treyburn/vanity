e38b6c0c30e4a128ba0dd73c46b75a0a7b235356
Travis Reyburn <travisreyburn@gmail.com>
2026-04-04 12:26:23 -0600

    make use of error codes

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"os"
+
 	"github.com/alecthomas/kong"
 
 	"go.treyburn.dev/vanity/internal/cmd"
@@ -8,5 +11,10 @@ import (
 
 func main() {
 	ctx := kong.Parse(new(cmd.CLI))
-	ctx.FatalIfErrorf(ctx.Run())
+	err := ctx.Run()
+	if errors.Is(err, cmd.ErrValidationFailed) {
+		// TODO - print validation errors here? or elsewhere?
+		os.Exit(2)
+	}
+	ctx.FatalIfErrorf(err) // exit(0) if successful, exit(1) on other errors, exit(80) on cli misuse
 }
