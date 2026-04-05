6349461522bd06b82670f08ebcfd996884665119
Travis Reyburn <travisreyburn@gmail.com>
2026-04-05 11:31:02 -0600

    Dont walk relative directories

--- internal/vcs/git.go
+++ internal/vcs/git.go
@@ -124,9 +124,7 @@ func walkDir(bfs billy.Filesystem, dir string, seen map[string]bool) error {
 func shouldSkipDir(path string) bool {
 	name := filepath.Base(path)
 	switch name {
-	case ".", "..":
-		return false
-	case "vendor", "testdata", ".git":
+	case ".", "..", "vendor", "testdata", ".git":
 		return true
 	}
 	return strings.HasPrefix(name, ".")
