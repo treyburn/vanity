e26858031fb8f794043b58f7e7b3d4d3bcf46ae6
Travis Reyburn <travisreyburn@gmail.com>
2026-04-07 13:01:22 -0600

    Add a small blurb

--- doc.go
+++ doc.go
@@ -1,3 +1,5 @@
-// Package main is the entry point for the vanity CLI, a static site
-// generator for Go vanity import URLs.
+// Package main is the entry point for the Vanity CLI, a static site
+// generator for vanity Go module URLs.
+//
+// Install Vanity via go install go.treyburn.dev/vanity and take ownership of your Go module namespace.
 package main
